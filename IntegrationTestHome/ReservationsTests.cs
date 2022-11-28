b046f1edc16c02e6ce19dd7377d27daf2e6b4474
unknown <facebian@gmail.com>
2022-11-28 09:58:14 -0500

    Remove unncesary code

--- IntegrationTestHome/ReservationsTests.cs
+++ IntegrationTestHome/ReservationsTests.cs
@@ -13,29 +13,29 @@ namespace IntegrationTestHome
     public sealed class ReservationsTests
     {
 
-        [Fact]
-        public async Task PostValidReservation() 
-        {
-            var response = await PostReservation(new { 
-                date = "2023-03-10 19:00",
-                email = "edwin@example.com",
-                name = "Edwin Mendez",
-                quantity = 2
-            });
+    //    [Fact]
+    //    public async Task PostValidReservation() 
+    //    {
+    //        var response = await PostReservation(new { 
+    //            date = "2023-03-10 19:00",
+    //            email = "edwin@example.com",
+    //            name = "Edwin Mendez",
+    //            quantity = 2
+    //        });
 
-            Assert.True(response.IsSuccessStatusCode, $"Actual status code: {response.StatusCode}.");
-        }
+    //        Assert.True(response.IsSuccessStatusCode, $"Actual status code: {response.StatusCode}.");
+    //    }
 
-        [SuppressMessage("Usage", "CA2234: Pass system uri objects instead of strings", Justification = "URL isn't passed as variable, but as literal.")]
-        private async Task<HttpResponseMessage> PostReservation(object reservation)
-        {
-            using var factory = new WebApplicationFactory<Startup>();
-            var client = factory.CreateClient();
+    //    [SuppressMessage("Usage", "CA2234: Pass system uri objects instead of strings", Justification = "URL isn't passed as variable, but as literal.")]
+    //    private async Task<HttpResponseMessage> PostReservation(object reservation)
+    //    {
+    //        using var factory = new WebApplicationFactory<Startup>();
+    //        var client = factory.CreateClient();
 
-            string json = JsonSerializer.Serialize(reservation);
-            using var content = new StringContent(json);
-            content.Headers.ContentType.MediaType = "applicaiton/json";
-            return await client.PostAsync("reservations", content);
-        }
-    }
+    //        string json = JsonSerializer.Serialize(reservation);
+    //        using var content = new StringContent(json);
+    //        content.Headers.ContentType.MediaType = "applicaiton/json";
+    //        return await client.PostAsync("reservations", content);
+    //    }    
+    //}
 }
